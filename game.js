003cd99c47e79b4a71d8c5b3c6849b732ebfbe9a
tinonino <alex_valentino@abv.bg>
2018-05-28 21:21:41 +0300

    new game

--- game.js
+++ game.js
@@ -1,14 +1,48 @@
 ﻿// Creating variables
 var myX = 0, myY = 0;
+var vragX = 900, vragY = 300;
+var isGameOver=false;
+var lives=3;
 
 function update() {
-    myX = myX+(mouseX-myX)/10;
-    myY = myY+(mouseY-myY)/10;
+    if(isGameOver==false){
+        vragX-=3;
+        if(vragX<=0){
+            vragX=900;
+            vragY=Math.random()*550;
+        }
+
+        if(isKeyPressed[38]){
+            myY-=3;
+        }
+        if(isKeyPressed[40]){
+            myY+=3;
+        }
+    }
+    
+    if(areColliding(myX, myY, 30, 30, vragX, vragY, 50, 50)){
+        lives--;
+        vragX=900;
+        vragY=Math.random()*550;
+    }
+    if(lives==0){
+        isGameOver=true;
+    }
 }
 
 function draw() {
     // This is how you draw a rectangle
+    context.fillStyle="blue";
     context.fillRect(myX, myY, 30, 30);
+    context.fillStyle="red";
+    context.fillRect(vragX, vragY, 50, 50);
+    if(isGameOver==true){
+        context.fillStyle="black";
+        context.fillRect(0, 0, 800, 600);
+        context.fillStyle="white";
+        context.font="50px Arial";
+        context.fillText("GAME OVER", 220, 300);
+    }
 };
 
 function keyup(key) {
